72efaf1a94609e0b5f71fea3f06f80b58b3376fc
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-07-04 04:40:07 +0530

    environment and platform check

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== " production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+const isWindows = process.platform === "win32" ? true : false;
+
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "ImageShrink",
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
   });
+
+  //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile("./app/index.html");
 }
 
 // use cmd+shift+i for accessing dev tools
 app.on("ready", createMainWindow);
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
