d707f2560912685697b238da8d59a1acce5a077a
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-07-20 22:26:29 +0530

    implemented imagemin

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+const path = require("path");
+const os = require("os");
 const {
   app,
   BrowserWindow,
   Menu,
   globalShortcut,
   ipcMain,
+  shell,
 } = require("electron");
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const slash = require("slash");
 
 process.env.NODE_ENV = "development";
 
@@ -116,9 +123,32 @@ const menu = [
     : []),
 ];
 
-ipcMain.on("image:minimize", (e, options) => {
-  console.log(options);
+ipcMain.on("image:minimize", async (e, options) => {
+  // console.log(options);
+  options.dest = path.join(os.homedir(), "imageshrink");
+  shrinkImage(options);
 });
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+
+    console.log(files);
+
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 app.on("window-all-closed", () => {
   if (!isMac) {
